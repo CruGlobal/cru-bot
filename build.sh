4e49baa34e411bb3ddcba5cee1532b704c4f55d4
Josh Starcher <josh.starcher@gmail.com>
2017-10-26 13:29:14 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 git submodule update --init --recursive
 
 `/usr/local/bin/aws ecr get-login` &&
-docker build -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/cru-bot:$GIT_COMMIT-$BUILD_NUMBER .
+docker build -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/cru-bot:$ENVIRONMENT-$BUILD_NUMBER .
